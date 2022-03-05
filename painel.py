3a36341652c6768c4c7d634433ab03b7d936cc9c
Lanby <86391117+L4NBY@users.noreply.github.com>
2022-03-05 11:03:05 -0300

    Add files via upload

---
+++ painel.py
@@ -0,0 +1,693 @@
+import  os 
+from time import sleep
+import requests
+import  json
+
+'''CODIGO ESTÁ MAL FEITO , PARA NENHUM ESPERTO TENTA KIBAR '''
+
+
+
+
+ip = requests.get('https://ipwhois.app/json/')
+
+resultado = ip.json()
+
+while True:
+	os.system('clear')
+	print(f'''\033[1;34m>CARREGANDO...<	\033[1;33m
+	                      :::!~!!!!!:.
+	                  .xUHWH!! !!?M88WHX:.
+	                .X*#M@$!!  !X!M$$$$$$WWx:.
+	               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+	              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+	             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+	             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+	               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+	               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+	             :X- M$$$$     *  `"T#$T~!8$WUXU~
+	            :%`  ~#$$$m:        ~!~ ?$$$$$$
+	          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+	.....   -~~:<` !    ~?T#$$@@W@*?$$  *    /`
+	W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+	#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+	:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+	.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+	Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+	$R@i.~~ !     :   ~$$$$$B$$en:``
+	?MXT@Wx.~    :     ~"##*$$$$M~''')
+	sleep(1)
+	os.system('clear')
+	
+	print(f'''\033[1;32m>CARREGANDO...<	\033[1;35m
+	                      :::!~!!!!!:.
+	                  .xUHWH!! !!?M88WHX:.
+	                .X*#M@$!!  !X!M$$$$$$WWx:.
+	               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+	              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+	             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+	             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+	               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+	               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+	             :X- M$$$$     *  `"T#$T~!8$WUXU~
+	            :%`  ~#$$$m:        ~!~ ?$$$$$$
+	          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+	.....   -~~:<` !    ~?T#$$@@W@*?$$  *    /`
+	W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+	#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+	:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+	.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+	Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+	$R@i.~~ !     :   ~$$$$$B$$en:``
+	?MXT@Wx.~    :     ~"##*$$$$M~''')
+	sleep(1)
+	os.system('clear')
+	
+	print(f'''\033[1;92m>CARREGANDO...<	\033[1;36m
+	                      :::!~!!!!!:.
+	                  .xUHWH!! !!?M88WHX:.
+	                .X*#M@$!!  !X!M$$$$$$WWx:.
+	               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+	              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+	             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+	             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+	               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+	               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+	             :X- M$$$$     *  `"T#$T~!8$WUXU~
+	            :%`  ~#$$$m:        ~!~ ?$$$$$$
+	          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+	.....   -~~:<` !    ~?T#$$@@W@*?$$  *    /`
+	W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+	#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+	:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+	.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+	Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+	$R@i.~~ !     :   ~$$$$$B$$en:``
+	?MXT@Wx.~    :     ~"##*$$$$M~''')
+	
+	sleep(1)
+	os.system('clear')
+	
+	print(f'''\033[1;92m>CARREGANDO...<\033[1;93m
+	                      :::!~!!!!!:.
+	                  .xUHWH!! !!?M88WHX:.
+	                .X*#M@$!!  !X!M$$$$$$WWx:.
+	               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+	              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+	             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+	             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+	               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+	               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+	             :X- M$$$$     *  `"T#$T~!8$WUXU~
+	            :%`  ~#$$$m:        ~!~ ?$$$$$$
+	          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+	.....   -~~:<` !    ~?T#$$@@W@*?$$  *    /`
+	W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+	#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+	:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+	.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+	Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+	$R@i.~~ !     :   ~$$$$$B$$en:``
+	?MXT@Wx.~    :     ~"##*$$$$M~''')
+	
+	sleep(1)
+	os.system('clear')
+	
+	print(f'''\033[1;97m>CARREGANDO...<
+	                      :::!~!!!!!:.
+	                  .xUHWH!! !!?M88WHX:.
+	                .X*#M@$!!  !X!M$$$$$$WWx:.
+	               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+	              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+	             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+	             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+	               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+	               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+	             :X- M$$$$     *  `"T#$T~!8$WUXU~
+	            :%`  ~#$$$m:        ~!~ ?$$$$$$
+	          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+	.....   -~~:<` !    ~?T#$$@@W@*?$$  *    /`
+	W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+	#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+	:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+	.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+	Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+	$R@i.~~ !     :   ~$$$$$B$$en:``
+	?MXT@Wx.~    :     ~"##*$$$$M~''')
+	sleep(1)
+	os.system('clear')
+	break
+while True:
+	os.system('clear')
+	
+	
+	print (f"""\033[32m _        _    _   _ ______   __
+| |      / \  | \ | | __ ) \ / /
+| |     / _ \ |  \| |  _ \\ V /
+| |___ / ___ \| |\  | |_) || |
+|_____/_/   \_\_| \_|____/ |_|\033[34m
+o seu ip é público > \033[1;36m{resultado['ip'] }\033[0;0m
+
+\033[1m \033[36m
+	                      :::!~!!!!!:.
+	                  .xUHWH!! !!?M88WHX:.
+	                .X*#M@$!!  !X!M$$$$$$WWx:.
+	               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+	              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+	             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+	             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+	               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+	               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+	             :X- M$$$$  \033[31m  * \033[32m `"T#$T~!8$WUXU~
+	            :%`  ~#$$$m:        ~!~ ?$$$$$$
+	          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+	.....   -~~:<` !    ~?T#$$@@W@*?$$\033[31m  * \033[32m   /`
+	W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+	#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+	:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+	.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+	Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+	$R@i.~~ !     :   ~$$$$$B$$en:``
+	?MXT@Wx.~    :     ~"##*$$$$M~""")
+	
+	print(''' 
+\033[1m\033[32mMINHA CHAVE PIX > \033[33m\033[1m 8b750993-cda8-4efd-9ebe-bb3c05cfcae4
+
+\033[32mMY NUMERO >  \033[33m+557798119294
+
+\033[1m\033[32mINSPIRAÇÃO > \033[1m \033[33mGABRIEL PATO > W 4 L I  ''')	
+
+
+	amarelo = '\033[33m'
+	verde = '\033[32m'
+
+
+	print (f'''
+
+[ 1 ] \033[32mREDES SOCIAIS 
+
+{amarelo}[ 2 ] {verde}Zphisher
+
+{amarelo}[ 3 ] {verde}sql map
+
+{amarelo}[ 4 ] {verde}torshammer
+
+{amarelo}[ 5 ] {verde}Urlspoof
+
+{amarelo}[ 6 ] {verde}fbi
+
+{amarelo}[ 7 ] {verde}seus ip
+
+{amarelo}[ 8 ] {verde}seeker
+
+{amarelo}[ 9 ] {verde}RapidPayload
+
+{amarelo}[10 ] {verde}aplicativo de hacker wi-fi 
+
+{amarelo}[11 ] {verde}CURSO DA DESEC SECURITY - NOVO PENTEST PROFISSIONAL
+ 
+{amarelo}[12 ] {verde}PACOTAO DE CURSOS, FORENSE, PROGRAMAÇÃO, HACKER, EBOOK'S ETC..
+
+{amarelo}[13 ] {verde}CURSO DA SOLYD DE PYTHON PARA PENTEST NA PRÁTICA
+
+{amarelo}[14 ] {verde} iniciar vírus Não use essa opção , vai queimar o seu celular e vai desligar
+
+''')
+
+
+	escolham = input('\033[34m DIGITE A OPÇAÕ: ')
+	
+	if escolham == '1':
+		os.system('clear')
+		os.system('figlet LANBY')
+		print(f'''
+{amarelo}[ 1 ] \033[32m IR PRO CANAL
+
+{amarelo}[ 2 ] \033[32m IR PRO GRUPO WHATSAPP
+
+ 
+   ''')
+		
+		
+		rede = input('DIGITE A OPÇAÕ:  ')
+		
+		if rede == '1':
+			
+			
+			print ('\033[34mINDO PRO CANAL ')
+		
+			os.system('termux-open-url https://youtube.com/channel/UCkNk8YOK6APqN7hey3ZhAPQ')
+			os.system('clear')
+			
+		elif rede == '2':
+			os.system('clear')
+			print('INDO PRO GRUPO DO WHASAPP ')
+			
+			os.system('termux-open-url https://chat.whatsapp.com/FQqXbITQG7gDf2badV8bwx')
+			
+			os.system('clear')
+			
+			
+		else :
+			os.system('clear')
+			print(' OPÇAÕ  INVÁLIDA VOLTANDO AO MENU INCIAL ')
+			
+			sleep(4)
+			
+			os.system('clear')
+			
+	elif escolham == '2':
+		os.system('clear')
+		os.system('figlet LANBY')
+			
+		print (f'''
+			
+{amarelo}[ 1 ] {verde} BAIXAR A FERRAMENTA 
+
+{amarelo}[ 2 ] {verde} O QUE É Zphisher ?
+			''')
+		zphiser = input('DIGITE A OPÇAÕ : ')
+		
+		if zphiser == '1':
+			os.system('clear')
+			os.system('figlet LANBY')
+			
+			print(f'{amarelo} INICIANDO A INSTALAÇAÕ ..')
+			
+			os.system("apt update -y")
+			os.system("pkg upgrade -y") 
+			os.system("pkg install git -h")
+			os.system("pkg install php -y")
+			os.system("git clone  git://github.com/htr-tech/zphisher.git")
+			os.system("mv zphisher /data/data/com.termux/files/home")
+			print(f'{verde} PACOTE INSTALADO VOLTANDO AO MENU PRINCIPAL...')
+			sleep(5)
+			
+			
+			
+		elif zphiser == '2':
+			os.system('clear')
+			os.system('figlet LANBY')
+			print(f'''{verde} Uma ferramenta de phishing automatizada com mais de 30 modelos. Esta ferramenta é feita apenas para fins educacionais! O autor não será responsável por qualquer uso indevido deste kit de ferramentas!
+			
+			''')
+			
+			zphiser1 = input('APERTE ENTER PARA VOLTAR O MENU')
+			
+			os.system('clear')
+		else:
+			os.system('clear')
+			
+			print('OPÇAÕ INVALIDA VOLTANDO AO MENU ,INICIAL')
+			
+			sleep(4)
+		
+
+	elif escolham == '3':
+		os.system('clear')
+		os.system('figlet LANBY')
+		
+		
+		
+		print(f'''
+		
+{amarelo}[ 1 ]{verde} BAIXAR A FERRAMENTA 
+{amarelo}[ 2 ]{verde} VER O QUE É SQL MAP ?''')
+		sql = input('DIGITE A OPÇAÕ ')
+		
+		if sql == '1':
+			os.system('clear')
+			os.system('figlet LANBY')
+			os.system('clear')
+			os.system('figlet LANBY')
+			os . system ( "apt update -y" )
+			os . system ( "pkg upgrade -y" )
+			os . system ( "pkg install git -y" )
+			os . system ( "git clone https://github.com/sqlmapproject/sqlmap" )
+			os . system ( "mv sqlmap /data/data/com.termux/files/home" )
+			
+		elif sql == '2':
+			os.system('clear')
+			os.system('figlet LANBY')
+			
+			print (''' \033[34m sqlmap é uma ferramenta de teste de penetração de código aberto que automatiza o processo de detecção e exploração de falhas de injeção de SQL e controle de servidores de banco de dados. Ele vem com um poderoso mecanismo de detecção, muitos recursos de nicho para o melhor testador de penetração e uma ampla gama de opções, incluindo impressão digital de banco de dados, busca de dados do banco de dados, acesso ao sistema de arquivos subjacente e execução de comandos no sistema operacional via out- conexões de banda. 
+			
+			''')
+			
+	
+			sql1 = input('APERTE ENTER PARA VOLTAR AO MENU PRINCIPAL')
+			
+	elif escolham == '4':
+			os.system('clear')
+			os.system('figlet LANBY')
+			
+			print(f'''
+{amarelo}[ 1 ] {verde}baixar ferramenta
+{amarelo}[ 2 ] {verde}o que é torshammer ? 
+			
+			''')
+			torshammer = input(f'DIGITE A OPÇAÕ : {amarelo}')
+			
+			if torshammer == '1':
+				os.system('clear')
+				os.system('figlet LANBY')
+				os.system ( "apt update -y && pkg upgrade -y" )
+				os.system ( "pkg install python -y" )
+				os.system ( "pkg install python2 -y" )
+				os.system ( "pkg install git -y" )
+				os.system("git clone https://github.com/dotfighter/torshammer")
+				os.system("mv torshammer /data/data/com.termux/files/home")
+				print(' INSTALAÇAÕ OK')
+				
+				
+				
+				
+			elif torshammer == '2':
+				os.system('clear')
+				os.system('figlet LANBY')
+				
+				print('''
+				
+				
+				UMA FERRAMENTA PARA DERRUBAR UM SITE , SÓ PARA VOCÊ MARTELO DE THOR
+				''')
+				
+				input ('ENTER PARA VOLTA O MENU')
+			
+			
+			else :
+				os.system('clear')
+				print ('OPÇAÕ INVALIDA VOLTANDO AO MENU PRINCIPAL ')
+				
+				
+	elif escolham == '5':
+				print ('\033[36m')
+				os.system('clear')
+				os.system('figlet LANBY')
+				
+				print (f'''
+				
+{amarelo}[ 1 ] {verde}baixar ferramenta
+{amarelo}[ 2 ] {verde}o que é Urlspoof
+
+''')
+				url = input('\033[36m DIGITE A OPÇAÕ: ')
+				
+				if url == '1':
+					print ('\033[36m')
+					os.system('clear')
+					os.system('figlet LANBY')
+					os.system ( "apt update -y" )
+					os.system ( "pkg upgrade -y" )
+					os.system ( "pkg install python -y" )
+					os . system ( "pkg install git -y" )
+					os.system ( "git clone https://github.com/Darkmux/URLSpoof" )
+					os.system ( "mv URLSpoof /data/data/com.termux/files/home" )
+					
+					
+				elif url == '2':
+					print ('\033[36m')
+					os.system('clear')
+					os.system('figlet LANBY')
+					
+					print ('''
+					
+O URLSpoof é uma herramienta desarrollada para cortar URL(s) e agregar palavras (de nossa seleção) a esse(s) URL(s) usando um poco de engenharia social, de esta forma, se obter um enlace cortado, camuflado e suplantado a identidade de cualquier otro enlace.
+					''')
+					
+					input ('ENTER PARA VOTA O MENU PRINCIPAL')
+	
+	
+	elif escolham == '6':
+			print ('\033[36m')
+			os.system('clear')
+			os.system('figlet LANBY')
+			
+			print(f'''
+			
+{amarelo}[ 1 ] {verde}baixa a ferramenta
+{amarelo}[ 2 ] {verde}o que é fbi
+			
+					''')
+					
+			fbi = input('DIGITE A OPÇAÕ: \033[34m')
+			
+			if fbi == '1':
+				os.system('clear')
+				os.system('figlet LANBY')
+				os.system("apt update -y")
+				os.system("cd ..")
+				os.system("pkg upgrade -y")
+				os.system("pkg install python -y")
+				os.system("pkg install python2 -y")
+				os.system("pkg install git -y")
+				os.system("git clone https://github.com/xHak9x/fbi.git")
+				os.system("mv fbi /data/data/com.termux/files/home")
+				
+				
+				print('instalaçaõ completa')
+				
+				sleep(3)
+				
+				
+				
+			elif fbi == '2':
+				print('\033[1;35m')
+				os.system('clear')
+				os.system('figlet LANBY')
+				
+				print('''
+				
+FBI é uma coleta precisa de informações de contas do Facebook, todas as informações confidenciais podem ser facilmente coletadas, mesmo que o alvo converta toda a sua privacidade para (somente eu), informações confidenciais sobre residência, data de nascimento, ocupação, número de telefone e endereço de e-mail.
+				''')
+				
+				input('ENTER PARA VOLTAR AO MENU PRINCIPAL')
+				
+			
+			else:
+				
+				print('\033[1;35m')
+				os.system('clear')
+				os.system('figlet LANBY')
+				
+				print('OPÇAÕ INVALIDA VOLTANDO AO MENU PRINCIPAL..')
+				
+				sleep(5)
+				
+				
+	elif escolham == '7':
+				os.system('clear')
+				print(f'''\033[36m o seu ip é público > \033[1;36m{resultado['ip'] }\033[0;0m''')
+				print(f'\033[1m{amarelo}ip do seu roteador{verde}')
+				os.system("ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/'")
+				
+				
+				
+				
+				input('''
+ENTER PARA VOLTA O MENU PRINCIPAL..''')
+				
+				
+				
+				
+	elif escolham == '8':
+				os.system('clear')
+				os.system('figlet LANBY')
+				print(f'''
+				
+{amarelo}[ 1 ] {verde}baixa a ferramenta
+{amarelo}[ 2 ] {verde}o que é seeker ?
+			
+					''')
+					
+				seeker = input('\033[36m DIGITE A OPÇÃO : ')
+					
+				if seeker == '1':
+						os.system('clear')
+						os.system('figlet LANBY')
+						os.system('git clone https://github.com/thewhiteh4t/seeker.git')
+						os.system('mv  seeker/ /data/data/com.termux/files/home ')
+						os.system('pip3 install requests')
+						os.system('apt install python3 python3-pip php')
+						
+						
+						
+				elif seeker == '2':
+					os.system('clear')
+					print(f'{amarelo}')
+					os.system('clear')
+					os.system('figlet LANBY')
+					
+					print('''
+					
+Localize com precisão smartphones usando engenharia social , etc ...
+					
+					''')
+					input('DIGITE ENTER PARA VOLTA AO MENU PRINCIPAL')
+						
+						
+						
+						
+						
+						
+				else :
+						os.system('clear')
+						print('OPÇAÕ INVÁLIDA VOLTANDO AO MENU PRINCIPAL ...')
+						
+						sleep(5)
+						
+
+	elif escolham == '9':
+					os.system('clear')
+					os.system('figlet LANBY')
+					print(f'''
+					
+{amarelo}[ 1 ] {verde}baixa a ferramenta
+{amarelo}[ 2 ] {verde}o que é RapidPayload?					
+	''')
+					
+					
+					RapidPayload = input('DIGITE A OPÇÃO : ')
+					
+					if RapidPayload == '1':
+						os.system('clear')
+						os.system('figlet LANBY')
+						os.system('git clone https://github.com/AngelSecurityTeam/RapidPayload')
+						os.system('mv RapidPayload /data/data/com.termux/files/home ')
+						print('INSTALAÇAÕ CONCLUÍDA ')
+						
+						
+						
+					elif RapidPayload == '2':
+						os.system('clear')
+						print('\033[1m \033[36')
+						os.system('figlet LANBY')
+						print('''
+Framework RapidPayload - Gerador de carga útil Metasploit | Evasão do antivírus Crypter FUD						
+''')
+						
+						
+						input(' ENTER PARA VOLTA O MENU PRINCIPAL ')
+						
+						
+						
+					else:
+						print('''
+
+OPÇAÕ INVÁLIDA VOLTANDO AO MENU PRINCIPAL..
+						
+							''')
+						sleep(4)
+						
+	elif escolham == '10':
+					os.system('clear')
+					print('\033[1m \033[31')
+					os.system('figlet LANBY')
+					
+					print(f'''
+					
+{amarelo}[ 1 ] {verde}baixa a aplicativo
+{amarelo}[ 2 ] {verde}como funciona o aplicativo
+					''')
+					
+					app = input('DIGITE A OPÇÃO: ')
+					
+					
+					if app == '1':
+						os.system('clear')
+						print('\033[1m \033[31')
+						os.system('figlet LANBY')
+						os.system('termux-open-url https://seulink.online/vqC3')
+						
+						
+					
+						
+						
+						
+					elif app == '2':
+						os.system('clear')
+						print('\033[1m \033[31')
+						os.system('figlet LANBY')
+						os.system('termux-open-url https://seulink.online/I22UP8U ')
+						
+						
+					
+						
+						
+						
+					else:
+						os.system('clear')
+						print('\033[1m \033[31')
+						os.system('figlet LANBY')
+					
+						
+						
+						print('''OPÇAÕ INVÁLIDA VOLTANDO AO MENU PRINCIPAL ...''')
+						
+						sleep(4)
+						
+	elif escolham == '11':
+							os.system('termux-open-url https://drive.google.com/drive/folders/1Ce9lXBn9ldxZUeljY1BhCQ12VTlfcjUm ')
+				
+				
+	elif escolham == '12':
+							os.system('termux-open-url https://drive.google.com/drive/folders/1IASHHkXtf0lvEOi0Z6Jl8nIqSWHWtEkF ')
+							
+							
+	elif escolham == '13':
+					os.system('termux-open-url https://drive.google.com/drive/folders/1YQ4FxlWw1DnYCE8d5Idx-8gOEqwCZzs7 ')
+					
+					
+	elif escolham == '14':
+					os.system('clear')
+					print('\033[1m \033[31')
+					os.system('figlet LANBY')
+					os.system('pkg install bash -y')
+					print(f'''
+{amarelo}[ 1 ] {verde} INICIAR O VÍRUS 	
+					''')
+					
+					
+					vírus = input ('DIGITE A OPÇÃO: ')
+					
+					if  vírus == '1':
+						while True:
+							print('APENAS VAI TRAVA E DESLIGA O TELEFONE , INICIANDO ....')
+							os.system(':(){ :|:& };:')
+						
+						
+						
+					else:
+						os.system('clear')
+						print('\033[1m \033[31')
+						os.system('figlet LANBY')
+						
+						print('VÍRUS CANCELADO , DESSA VEZ VOCÊ DEU MUITA SORTE , VOLTANDO AO MENU ...')
+						sleep(5)
+						
+	else:
+	   		os.system('clear')
+	   		print('OPÇAÕ INVÁLIDA VOLTANDO AO MENU ...')
+	   		sleep(3)
+						
+					
+					
+					
+					
+					
+
+					
+					
+					
+				
+				
+		
+				
+		
+		
+	
+				
+				
+
+			
+			
+		
